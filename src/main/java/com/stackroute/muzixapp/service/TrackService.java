0c02b9c3e50dfc6413b1f866173dc6189ae368cc
vanufanofsk <b.vanmathi97@gmail.com>
2019-08-07 16:06:15 +0530

    SeedData using @Value,@PropertySource and global issues fixed

---
+++ src/main/java/com/stackroute/muzixapp/service/TrackService.java
@@ -0,0 +1,19 @@
+package com.stackroute.muzixapp.service;
+
+import com.stackroute.muzixapp.domain.Track;
+
+import java.util.List;
+
+public interface TrackService {
+    public Track saveTrack(Track track);
+
+    public List<Track> getAllTracks();
+
+    public Track getTrackById(int id);
+
+    public Track deleteTrackById(int id);
+
+    public List<Track> deleteAllTracks();
+
+    public Track updateById(int id, Track track);
+}
